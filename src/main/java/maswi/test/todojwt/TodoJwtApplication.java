44679004923873028bc708573374d9f8c9d9f46b
Maswi, MR <rmaswi360@outlook.com>
2022-07-06 00:11:41 +0300

    Initilized the project

---
+++ src/main/java/maswi/test/todojwt/TodoJwtApplication.java
@@ -0,0 +1,13 @@
+package maswi.test.todojwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maswi/test/todojwt/TodoJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package maswi.test.todojwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
