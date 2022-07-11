74cd60e502186d9adecda41880a6db39084ff31f
Maswi, MR <rmaswi360@outlook.com>
2022-07-11 07:29:10 +0300

    Created jwt auth

---
+++ src/main/java/maswi/test/todojwt/controllers/AuthenticationController.java
@@ -0,0 +1,52 @@
+package maswi.test.todojwt.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import maswi.test.todojwt.components.JwtUtils;
+import maswi.test.todojwt.models.JwtRequest;
+import maswi.test.todojwt.models.User;
+import maswi.test.todojwt.repositories.UserRepository;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/login")
+public class AuthenticationController {
+  @Autowired
+  private AuthenticationManager authenticationManager;
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Autowired
+  private JwtUtils jwtUtils;
+
+  @PostMapping
+  public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest jwtRequest) throws Exception {
+    authenticate(jwtRequest.getName(), "");
+    final List<User> userDetails = userRepository.findByName(jwtRequest.getName());
+    final String jwtToken = jwtUtils.generateToken(userDetails.get(0));
+    return ResponseEntity.ok(jwtToken);
+  }
+
+  private void authenticate(String username, String password) throws Exception {
+    try {
+      authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+    } catch (DisabledException e) {
+      throw new Exception("USER_DISABLED", e);
+    } catch (BadCredentialsException e) {
+      throw new Exception("INVALID_CREDENTIALS", e);
+    }
+  }
+}
---
+++ src/main/java/maswi/test/todojwt/models/JwtRequest.java
@@ -0,0 +1,12 @@
+package maswi.test.todojwt.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class JwtRequest {
+    String name;
+}
